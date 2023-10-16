3c79edeaf32f7ba54080e889066d209bc8bd4166
jmvigueras <jmvigueras@jmvigueras.com>
2023-10-16 16:10:23 +0200

    update ftg module

--- T1_day0_IaC_vpc_fgt_server/terraform/modules/fgt/iam.tf
+++ T1_day0_IaC_vpc_fgt_server/terraform/modules/fgt/iam.tf
@@ -1,8 +1,10 @@
+/*
 # Create the IAM role/profile for the API Call
 resource "aws_iam_instance_profile" "fgt-apicall-profile" {
   name = "${var.prefix}-fgt-apicall-profile"
   role = aws_iam_role.fgt-apicall-role.name
 }
+*/
 
 resource "aws_iam_role" "fgt-apicall-role" {
   name = "${var.prefix}-fgt-apicall-role"
